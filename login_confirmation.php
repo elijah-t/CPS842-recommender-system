a78f1ae82efde11c5f9db49be8021d4b14c35fb5
elijah-t <elijah.tungul@ryerson.ca>
2021-12-07 20:08:15 -0500

    Login fixes

--- login_confirmation.php
+++ login_confirmation.php
@@ -23,48 +23,48 @@
         <link rel="shortcut icon" type="image/jpg" href="favicon.ico"/>
     </head>
     <body>
-        <?php
-            $mysqli = new mysqli("localhost", "root", "", "recommender");
+    <?php
+        $mysqli = new mysqli("localhost", "root", "", "recommender");
 
-            function getColumn($data, $mysqli) {
-                $user = $mysqli->prepare("SELECT {$data} FROM users;");
-                $user->execute();
-                
-                $arr = [];
+        function getColumn($data, $mysqli) {
+            $user = $mysqli->prepare("SELECT {$data} FROM users;");
+            $user->execute();
+            
+            $arr = [];
 
-                foreach ($user->get_result() as $row){
-                    $arr[] = $row["{$data}"];
-                }
-
-                return $arr;
+            foreach ($user->get_result() as $row){
+                $arr[] = $row["{$data}"];
             }
 
-            $logins = array_combine(getColumn("username", $mysqli), getColumn("password", $mysqli));
+            return $arr;
+        }
 
-            function redirect($url, $permanent = false) {
-                if (headers_sent() === false) header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
-                exit();
-            }
+        $logins = array_combine(getColumn("username", $mysqli), getColumn("password", $mysqli));
 
-            if(isset($_POST["user"]) and isset($_POST["pass"])){
-                if(array_key_exists($_POST["user"], $logins) and $_POST["pass"] == $logins[$_POST["user"]]) {
-                    $_SESSION["user"] = $_POST["user"];
-                    $_SESSION["pass"] = $_POST["pass"];
-                } else {
-                    session_unset();
-                    session_destroy();
-                    redirect("incorrect_login.html");
-                }
-            } elseif(array_key_exists($_POST["user"], $logins) and $_POST["pass"] == $logins[$_POST["user"]]) {
-                while(true){
-                    break;
-                }
+        function redirect($url, $permanent = false) {
+            if (headers_sent() === false) header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+            exit();
+        }
+
+        if(isset($_POST["user"]) and isset($_POST["pass"])){
+            if(array_key_exists($_POST["user"], $logins) and $_POST["pass"] == $logins[$_POST["user"]]) {
+                $_SESSION["user"] = $_POST["user"];
+                $_SESSION["pass"] = $_POST["pass"];
             } else {
                 session_unset();
                 session_destroy();
-                redirect("incorrect.html");
+                redirect("incorrect_login.html");
             }
-        ?>
+        } elseif(array_key_exists($_POST["user"], $logins) and $_POST["pass"] == $logins[$_POST["user"]]) {
+            while(true){
+                break;
+            }
+        } else {
+            session_unset();
+            session_destroy();
+            redirect("incorrect.html");
+        }
+    ?>
 
         <?php
             if(isset($_SESSION["user"])){
